55cde08e387622a4798f91ad782efc8ec7e24a6d
Brandon Roberts <robertsbt@gmail.com>
2023-05-22 14:15:55 -0500

    feat: add page prerendering example

--- src/main.providers.ts
+++ src/main.providers.ts
@@ -2,4 +2,8 @@
  * Common providers shared with client and server-side.
  */
 
-export const mainProviders = [];
+import { provideContent, withMarkdownRenderer } from "@analogjs/content";
+
+export const mainProviders = [
+  provideContent(withMarkdownRenderer())
+];
